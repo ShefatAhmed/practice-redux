a7de0f12d17f3eef4e9ad9aa011ed45c4caa263e
unknown <shefata69@gmail.com>
2024-10-03 10:17:28 +0600

    complete increment decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { decrement, increment } from "./redux/features/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hook"
+
 function App() {
+  const {count} = useAppSelector((state) => state.counter)
+  const dispatch = useAppDispatch()
 
   return (
     <div className="h-screen flex justify-center items-center gap-5">
-      <button className="bg-green-400 px-5 py-1 text-white font-bold">Increment</button>
-      <h1>0</h1>
-      <button className="bg-red-400 px-5 py-1 text-white font-bold">Decrement</button>
+      <button onClick={()=> dispatch(increment())} className="bg-green-400 px-5 py-1 text-white font-bold">Increment</button>
+      <h1>{count}</h1>
+      <button onClick={()=> dispatch(decrement())} className="bg-red-400 px-5 py-1 text-white font-bold">Decrement</button>
     </div>
   )
 }
