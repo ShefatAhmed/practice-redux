db88f913540458008058885830258b16dc0683f6
unknown <shefata69@gmail.com>
2024-10-03 09:59:40 +0600

    design complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
 
   return (
-    <></>
+    <div className="h-screen flex justify-center items-center gap-5">
+      <button className="bg-green-400 px-5 py-1 text-white font-bold">Increment</button>
+      <h1>0</h1>
+      <button className="bg-red-400 px-5 py-1 text-white font-bold">Decrement</button>
+    </div>
   )
 }
 
